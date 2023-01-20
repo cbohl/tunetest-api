4ba9c4abbf0d50b79ec076ee907c336a37da93e2
Christopher Bohl <chrisrbohl@gmail.com>
2023-01-20 12:48:52 -0600

    Refactors App.ts.

--- dist/app.js
+++ dist/app.js
@@ -18,7 +18,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const pg_1 = require("pg");
 const client_1 = require("@prisma/client");
 const express_graphql_1 = require("express-graphql");
-// import { buildSchema, BuildSchemaOptions } from "graphql";
 const schema_1 = require("@graphql-tools/schema");
 const cors_1 = __importDefault(require("cors"));
 const graphql_log_1 = __importDefault(require("graphql-log"));
@@ -38,16 +37,6 @@ const connectToDB = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 connectToDB();
-// const schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-// const root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-// };
 const app = (0, express_1.default)();
 dotenv_1.default.config(); //Reads .env file and makes it accessible via process.env
 const prisma = new client_1.PrismaClient();
@@ -87,14 +76,6 @@ const typeDefs = `
     createScoreRecord(artistId: Int, username: String, score: Int): ScoreRecord
   }
   `;
-// query superArtists: artists{
-//   songs {
-//     title
-//   }
-// }
-// type Mutation {
-//   createSpecial(name: String!, email: String!): User!
-// }
 const resolvers = {
     Query: {
         allUsers: () => {
@@ -121,14 +102,6 @@ const resolvers = {
     },
     Mutation: {
         createScoreRecord: (_, args) => {
-            // const newScoreRecord = prisma.scoreRecord.create({
-            //     data: {
-            //       artistId: data.artistId,
-            //       username: data.username,
-            //       score: data.score
-            //     }
-            //   })
-            // }
             return prisma.scoreRecord.create({
                 data: {
                     artistId: args.artistId,
@@ -139,23 +112,11 @@ const resolvers = {
         }
     }
 };
-// Mutation: {
-//   createSpecial: (parent: any, args: any) => {
-//     return prisma.user.create({
-//       email: "special@speciallly.com",
-//       name: "SuperSpecialName"
-//     });
-//   }
-// }
 const schema = (0, schema_1.makeExecutableSchema)({
     resolvers,
     typeDefs,
 });
-// Create a logger
 const logExecutions = (0, graphql_log_1.default)();
-// Wrap your resolvers
-console.log('log executions', logExecutions(resolvers));
-// app.use(cors())
 app.use((0, cors_1.default)({
     origin: '*'
 }));
@@ -164,103 +125,12 @@ app.use('/graphql', (0, express_graphql_1.graphqlHTTP)({
     schema: schema,
     graphiql: true,
     extensions({ result, }) {
-        console.log(result.data);
     },
-    // formatError: (err) => {
-    // return err.message
-    // }
 }));
-// app.use(function (req, res, next) {
-//   let originalSend = res.send;
-//   res.send = function (data) {
-//       console.log(data);
-//       originalSend.apply(res, Array.from(arguments));
-//   }
-//   next();
-// })
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
-// import express from 'express'
 app.get("/test", (req, res, next) => {
-    res.send("hi333555");
+    res.send("Test route");
 });
-app.get("/super", (req, res, next) => {
-    // const test = await prisma.user.create({
-    //   data: {email: 'testuser@testtest.com'}
-    // })
-    const cars = { "bmw": "super" };
-    res.json(cars);
-});
-// app.get("/users", (req: Request, res: Response, next: NextFunction) => {
-//   const users = await prisma.user.findMany({});
-//   res.json(users);
-// })
-app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const users = yield prisma.user.findMany();
-    console.log("users here", users);
-    res.json(users);
-    // const posts = await prisma.post.findMany()
-    // console.log(posts)
-    // res.json(users)
-}));
-app.get('/stellar', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const artists = yield prisma.artist.findMany({
-        include: { songs: true }
-    });
-    const songs = yield prisma.song.findMany();
-    console.log("artists here", artists);
-    res.json(artists);
-    // res.send("testing!")
-}));
-app.post('/post', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // const { title, content, authorEmail } = req.body
-    const email = "test22@test22.com";
-    const name = "Maggie Johnson";
-    const post = yield prisma.user.create({
-        data: {
-            email,
-            name
-        },
-    });
-    res.json(post);
-}));
-// const app = express()
-// app.get('/feed', async (req, res) => {
-//   const posts = await prisma.post.findMany({
-//     where: { published: true },
-//     include: { author: true },
-//   })
-//   res.json(posts)
-// })
-// app.put('/publish/:id', async (req, res) => {
-//   const { id } = req.params
-//   const post = await prisma.post.update({
-//     where: { id },
-//     data: { published: true },
-//   })
-//   res.json(post)
-// })
-// app.delete('/user/:id', async (req, res) => {
-//   const { id } = req.params
-//   const user = await prisma.user.delete({
-//     where: {
-//       id,
-//     },
-//   })
-//   res.json(user)
-// })
-// const server = app.listen(3000)
-// app.use(express.static('./public'))
-// app.use(express.static(path.join(__dirname, 'static')));
 app.use('/static', express_1.default.static('public'));
-// const path = require('path')
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-// const path = require('path')
-// app.use('/static', express.static(path.join(__dirname, 'public')))
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at ${process.env.PORT}`);
 });
-// app.listen(4000)
--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,9 @@ import dotenv from "dotenv";
 import { Pool } from "pg";
 import { PrismaClient } from '@prisma/client';
 import { graphqlHTTP } from "express-graphql";
-// import { buildSchema, BuildSchemaOptions } from "graphql";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import cors from "cors"
 import createGraphQLLogger from 'graphql-log'
-import { isAsteriskToken } from "typescript";
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -27,19 +25,6 @@ const connectToDB = async () => {
   }
 };
 connectToDB();
-
-// const schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-
-
-// const root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-// };
 
 const app = express();
 dotenv.config(); //Reads .env file and makes it accessible via process.env
@@ -83,17 +68,6 @@ const typeDefs = `
   }
   `;
   
-
-  // query superArtists: artists{
-  //   songs {
-  //     title
-  //   }
-  // }
-
-  // type Mutation {
-  //   createSpecial(name: String!, email: String!): User!
-  // }
-
   const resolvers = {
   Query: {
     allUsers: () => {
@@ -120,14 +94,6 @@ const typeDefs = `
   },
   Mutation: {
     createScoreRecord: (_, args) => {
-      // const newScoreRecord = prisma.scoreRecord.create({
-      //     data: {
-      //       artistId: data.artistId,
-      //       username: data.username,
-      //       score: data.score
-      //     }
-      //   })
-      // }
       return prisma.scoreRecord.create({
         data: {
           artistId: args.artistId,
@@ -138,27 +104,14 @@ const typeDefs = `
     }
   }
 };
-// Mutation: {
-//   createSpecial: (parent: any, args: any) => {
-//     return prisma.user.create({
-//       email: "special@speciallly.com",
-//       name: "SuperSpecialName"
-//     });
-//   }
-// }
 
 const schema = makeExecutableSchema({
   resolvers,
   typeDefs,
 });
 
-// Create a logger
 const logExecutions = createGraphQLLogger();
 
-// Wrap your resolvers
-console.log('log executions', logExecutions(resolvers));
-
-// app.use(cors())
 app.use(cors({
   origin: '*'
 }));
@@ -170,130 +123,15 @@ app.use('/graphql', graphqlHTTP({
   extensions({
     result,
   }) {
-    console.log(result.data);
   },
-  // formatError: (err) => {
-    // return err.message
-  // }
 }));
 
-// app.use(function (req, res, next) {
-//   let originalSend = res.send;
-//   res.send = function (data) {
-//       console.log(data);
-//       originalSend.apply(res, Array.from(arguments));
-//   }
-//   next();
-// })
-
-
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
-
-
-
-// import express from 'express'
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-  
-  res.send("hi333555");
+  res.send("Test route");
 });
 
-app.get("/super", (req: Request, res: Response, next: NextFunction) => {
-  
-
-  // const test = await prisma.user.create({
-  //   data: {email: 'testuser@testtest.com'}
-  // })
-  const cars = {"bmw": "super"};
-  res.json(cars);
-})
-
-// app.get("/users", (req: Request, res: Response, next: NextFunction) => {
-//   const users = await prisma.user.findMany({});
-//   res.json(users);
-// })
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  console.log("users here", users)
-  res.json(users)
-
-  // const posts = await prisma.post.findMany()
-  // console.log(posts)
-  // res.json(users)
-})
-app.get('/stellar', async (req, res) => {
-  const artists = await prisma.artist.findMany({
-    include: { songs: true }
-  })
-  const songs = await prisma.song.findMany()
-  console.log("artists here", artists)
-  res.json(artists)
-  // res.send("testing!")
-})
-
-app.post('/post', async (req, res) => {
-  // const { title, content, authorEmail } = req.body
-  const email = "test22@test22.com";
-  const name = "Maggie Johnson";
-  const post = await prisma.user.create({
-    data: {
-      email,
-      name
-    },
-  })
-  res.json(post)
-})
-
-// const app = express()
-
-// app.get('/feed', async (req, res) => {
-//   const posts = await prisma.post.findMany({
-//     where: { published: true },
-//     include: { author: true },
-//   })
-//   res.json(posts)
-// })
-
-
-// app.put('/publish/:id', async (req, res) => {
-//   const { id } = req.params
-//   const post = await prisma.post.update({
-//     where: { id },
-//     data: { published: true },
-//   })
-//   res.json(post)
-// })
-
-// app.delete('/user/:id', async (req, res) => {
-//   const { id } = req.params
-//   const user = await prisma.user.delete({
-//     where: {
-//       id,
-//     },
-//   })
-//   res.json(user)
-// })
-
-// const server = app.listen(3000)
-
-// app.use(express.static('./public'))
-
-// app.use(express.static(path.join(__dirname, 'static')));
 app.use('/static', express.static('public'))
-
-// const path = require('path')
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-
-// const path = require('path')
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at ${process.env.PORT}`);
 });
-
-// app.listen(4000)
